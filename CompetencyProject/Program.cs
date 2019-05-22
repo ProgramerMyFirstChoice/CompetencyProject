f6733922f22967e8dc9292381cd12997197f6d2b
Programer <programer2018@gmail.com>
2019-05-21 17:41:33 -0700

    adding comment statement on matchcollection variable called m1

--- CompetencyProject/Program.cs
+++ CompetencyProject/Program.cs
@@ -3,13 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace CompetencyProject
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // 1.
+            // URL: http://en.wikipedia.org/wiki/Main_Page
+            WebClient w = new WebClient();
+            string s = w.DownloadString("http://en.wikipedia.org/wiki/Main_Page");
+
+            // 2.
+            foreach (LinkItem i in LinkFinder.Find(s))
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
         }
     }
+
+    public struct LinkItem
+    {
+        public string Href;
+        public string Text;
+
+        public override string ToString()
+        {
+            return Href + "\n\t" + Text;
+        }
+    }
+
+    static class LinkFinder
+    {
+        public static List<LinkItem> Find(string file)
+        {
+            List<LinkItem> list = new List<LinkItem>();
+
+            // 1.
+            // Find all matches in file adn add the match object to Matchcollocetion
+            MatchCollection m1 = Regex.Matches(file, @"(<a.*?>.*?</a>)",
+                RegexOptions.Singleline);
+
+            // 2.
+            // Loop over each match.
+            foreach (Match m in m1)
+            {
+                string value = m.Groups[1].Value;
+                LinkItem i = new LinkItem();
+
+                // 3.
+                // Get href attribute.
+                Match m2 = Regex.Match(value, @"href=\""(.*?)\""",
+                    RegexOptions.Singleline);
+                if (m2.Success)
+                {
+                    i.Href = m2.Groups[1].Value;
+                }
+
+                // 4.
+                // Remove inner tags from text.
+                string t = Regex.Replace(value, @"\s*<.*?>\s*", "",
+                    RegexOptions.Singleline);
+                i.Text = t;
+
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+
 }
